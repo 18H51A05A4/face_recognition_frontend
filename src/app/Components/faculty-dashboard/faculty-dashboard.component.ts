79f8392ef07dd0cb7075f90ddbbb956bd18c163d
Juhitha_Reddy <juhitha.penumalli@techigai.io>
2022-05-27 14:20:21 +0530

    create form for adding class links

--- src/app/Components/faculty-dashboard/faculty-dashboard.component.ts
+++ src/app/Components/faculty-dashboard/faculty-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-faculty-dashboard',
@@ -8,19 +9,50 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class FacultyDashboardComponent implements OnInit {
 
   // @ViewChild('getAttendance') getAttendance: any;
-
+ @ViewChild('items') items:ElementRef<HTMLElement>;
+ @ViewChildren('studentList') studentList:QueryList<HTMLElement>
   showOverlay: boolean = false;
-
-  constructor() { }
-
+  showStudents:boolean=false;
+  myForm:FormGroup
+  students: any[] = [];
+  form:any[]=[];
+  chckedList:any[]=[];
+  constructor(private fb:FormBuilder) { }
   ngOnInit(): void {
+    this.students[0]={
+    id:1,
+    studentName:"venkatesh"
+    }
+    this.students[1]={
+    id:2,
+    studentName:"Sreejan"
+    }
+    this.myForm=this.fb.group({
+    className:[''],
+    startTime:[''],
+    endTime:[''],
+    date:[''],
+    })
   }
-
+  onSubmit(){
+    console.log(this.myForm)
+    this.studentList.forEach((item)=>{
+     console.log(item)
+    })
+  }
   getAttendance() {
     this.showOverlay = true;
   }
-
   closeModal() {
     this.showOverlay = false;
   }
+  displayStudents(){
+     if(this.items.nativeElement.style.display=='block')
+     this.items.nativeElement.style.display='none'
+     else
+     this.items.nativeElement.style.display='block'
+    console.log("toggle", this.items);
+  }
+
 }
+
